59117e524c61f9dd8f44a2eee5552602a6849cd5
akash rattan <akashrattan43@Gmail.com>
2020-10-29 16:15:23 -0700

    status

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
 const express = require('express')
+
 const bodyParser = require('body-parser')
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}))
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form  action="product" method="POST"><input type="text" name="title"><button type="submit">ADD PRODUCT</button></form>')
-})
+app.use(adminRoutes)
+app.use(shopRoutes)
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body)
-    res.redirect('/')
-})
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello from express!!</h1>')
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>')
 })
 
 app.listen(3000)
