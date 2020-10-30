873fd9d020214872f5a4e76f38558abd18eaa4c3
akash rattan <akashrattan43@Gmail.com>
2020-10-29 17:43:38 -0700

    html

--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,14 @@ const express = require('express')
 
 const router = express.Router()
 
+//get
 router.get('/add-product', (req, res, next) => {
-    res.send('<form  action="product" method="POST"><input type="text" name="title"><button type="submit">ADD PRODUCT</button></form>')
+    res.send('<form  action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">ADD PRODUCT</button></form>')
 })
 
-router.post('/product', (req, res, next) => {
+
+// post
+router.post('/add-product', (req, res, next) => {
     console.log(req.body)
     res.redirect('/')
 })
