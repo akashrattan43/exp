56f29441eda103c51c39aaf46fc1c6ab9cd06847
akash rattan <akashrattan43@Gmail.com>
2020-10-29 18:05:25 -0700

    path

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,13 @@
+const path = require('path')
 const express = require('express')
+
+const rootDir = require('../util/path')
 
 const router = express.Router()
 
 //get
 router.get('/add-product', (req, res, next) => {
-    res.send('<form  action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">ADD PRODUCT</button></form>')
+    res.sendFile(path.join(rootDir,  'views', 'add-product.html'))
 })
 
 
